6dbc25b9c375a6f98c3c958c1b6a6e267a706f2a
COLIN WOOD <copewood@gmail.com>
2022-09-11 18:24:11 +1000

    Initial commit from Create Next App

---
+++ pages/_app.tsx
@@ -0,0 +1,22 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from '../styles/globals.styles';
+import type { AppProps } from 'next/app';
+
+const theme = {
+  colors: {
+    primary: '#fafafa',
+  },
+};
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default MyApp;
